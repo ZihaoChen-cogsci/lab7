9e3510169327e1d833761d96d261eeee6d834c31
ZihaoChen-cogsci <63748182+ZihaoChen-cogsci@users.noreply.github.com>
2021-02-18 01:44:55 -0800

    Update introHCI.js
    1

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,10 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+	$(".likeBtn").click(click_count);
 	// your code here
+}
+function click_count(e){
+	e.preventDefault();
+	ga("send", "event", "like", "click");	
 }
